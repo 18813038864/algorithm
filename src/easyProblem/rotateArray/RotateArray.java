76ab7e2bdccbdf55abb1636fafeb197fd0b8794b
zengxinagji <zengxiangji@xdf.cn>
2018-10-17 09:01:00 +0800

    fix

---
+++ src/easyProblem/rotateArray/RotateArray.java
@@ -0,0 +1,54 @@
+package easyProblem.rotateArray;
+
+public class RotateArray {
+
+    public void rotate3(int[] nums, int k) {
+        int[] a = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            a[(i + k) % nums.length] = nums[i];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = a[i];
+        }
+    }
+
+    //Using Cyclic Replacements O(n)*O(n/k)
+    public void rotate2(int[] nums, int k) {
+        k = k % nums.length;
+        int count = 0;
+        for (int start = 0; count < nums.length; start++) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int next = (current + k) % nums.length;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+                count++;
+            } while (start != current);
+        }
+    }
+
+   //the best O(n),O(1)
+    public void rotate1(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args){
+
+    }
+}
