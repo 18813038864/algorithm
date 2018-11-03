698c2f91d36d3c8f671fc8d89314696655e2f76a
zengxiangji <1245768745@qq.com>
2018-11-03 16:38:00 +0800

    valid anagram

---
+++ src/easyProblem/deleteNodeInALinkedList/DeleteNodeInALinkedList.java
@@ -0,0 +1,22 @@
+package easyProblem.deleteNodeInALinkedList;
+
+
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+}
+
+
+public class DeleteNodeInALinkedList {
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    public static void main(String[] args){
+
+    }
+}
+
