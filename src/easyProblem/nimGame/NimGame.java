2b9bdc48ebf9e1d60d1bd7bda95f7f0e49afe6d3
zengxinagji <zengxiangji@xdf.cn>
2018-11-06 09:02:38 +0800

    nim game

---
+++ src/easyProblem/nimGame/NimGame.java
@@ -0,0 +1,10 @@
+package easyProblem.nimGame;
+
+public class NimGame {
+    public boolean canWinNim(int n) {
+        return n%4!=0;
+    }
+    public static void main(String[] args){
+
+    }
+}
